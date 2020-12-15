168c5ad37d83f067ea39ba347404d653f39335df
Karan Dumbre <Karan.dumbre@globant.com>
2020-12-15 23:41:21 +0530

    Major:[Little-Tags]:Project-Setup

---
+++ src/components/input/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// styles
+// Import Styles over here
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default Input;
